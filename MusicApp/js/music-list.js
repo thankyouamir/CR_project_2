d740d8272fecff5d7582a78119789f9f8b4558e0
thankyouamir <133989117+thankyouamir@users.noreply.github.com>
2023-10-12 20:56:33 +0530

    using HTML , CSS & JS

---
+++ MusicApp/js/music-list.js
@@ -0,0 +1,34 @@
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-2",
+    src: "music-2"
+  },
+  
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-3",
+    src: "music-3"
+  },
+  
+  
+  {
+    name: "BTS(방탄소년단)'Butter' ",
+    artist: "HYBE LABELS",
+    img: "music-4",
+    src: "music-4"
+  },
+  
+
+  
+];
